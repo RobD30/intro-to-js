c6e6a842e53d31c1836150a6bc37046e0376410c
Robert Dickerson <rdickeriii@gmail.com>
2017-10-02 01:04:07 -0700

    Refactor: More values and types
    end_of_line

--- values-types.js
+++ values-types.js
@@ -2,3 +2,17 @@
 42;
 // String, letters and characters. Can't do math with
 "42";
+
+a = 42;
+console.log(a);
+
+a = "42";
+console.log(a);
+
+alert(42);
+
+a = 42;
+a = a / 2;
+
+a = String(a);
+console.log(a);
