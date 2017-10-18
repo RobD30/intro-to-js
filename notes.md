6966127a1332680872e4c51542183bea82cd26fc
Robert Dickerson <rdickeriii@gmail.com>
2017-10-18 12:21:46 -0700

    Refactor:  More stuff and notes
    end_of_line

---
+++ notes.md
@@ -0,0 +1,3 @@
+#Notes
+
+// var types don't have a return value so they wont return output naturaly
--- operators.js
+++ operators.js
@@ -1,5 +1,17 @@
-a = 21;
+var a = 20;
 
 b = a * 2;
 
 console.log( b );
+
+a = a + 1;
+a = a * 2;
+
+console.log( a );
+
+// shortcuts
+
+a += 1;
+a *= 2;
+
+console.log(a);
