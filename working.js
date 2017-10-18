9c442a0e7fca5fe202b2b0e2b2460595df50d7dd
Robert Dickerson <rdickeriii@gmail.com>
2017-10-18 13:19:15 -0700

    Refactor:  It's a bad looooooop
    end_of_line

--- working.js
+++ working.js
@@ -1,3 +1,13 @@
-for (a = 0;1000000000000000; a += 1000){
-    console.log(a);
+for (a =5; a < 10; a += 1) {
+    a =6;
+    console.log(a)
+}
+
+a = 5;
+while (true) {
+    if (a >= 10) {
+        break;
+    }
+    console.log(a)
+    a += 1;
 }
